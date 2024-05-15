0a2076a2f6c58188f3fb3a24a725cce171be5a02
june <june.korbiz@gmail.com>
2024-05-16 00:39:13 +0900

    refactoring App.tsx

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./component/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -66,13 +65,4 @@ a {
 }
 `;
 
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <ToDoList />
-    </>
-  );
-}
-
-export default App;
+export default GlobalStyle;

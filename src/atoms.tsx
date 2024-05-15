e332208bae7742c242ed5c84930f5d7400233a09
june <june.korbiz@gmail.com>
2024-05-15 22:42:57 +0900

    feat categories enum

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,27 @@
 import { atom, selector } from "recoil";
 
+// type categories = "TO_DO" | "DOING" | "DONE";
+export enum Categories {
+    // "TO_DO",
+    // "DOING",
+    // "DONE",
+    "TO_DO" = "TO_DO",
+    "DOING" = "DOING",
+    "DONE" = "DONE",
+}
+
 export interface IToDo {
     text: string;
     id: number;
-    category: "TO_DO" | "DOING" | "DONE";
+    category: Categories;
 }
 
-// bubble
+export const categoryState = atom<Categories>({
+    key: "category",
+    // default: "TO_DO",
+    default: Categories.TO_DO,
+});
+
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
@@ -16,10 +31,7 @@ export const toDoSelector = selector({
     key: "toDoSelector",
     get: ({ get }) => {
         const toDos = get(toDoState);
-        return [
-            toDos.filter((toDo) => toDo.category === "TO_DO"),
-            toDos.filter((toDo) => toDo.category === "DOING"),
-            toDos.filter((toDo) => toDo.category === "DONE"),
-        ];
+        const category = get(categoryState);
+        return toDos.filter((toDo) => toDo.category === category);
     },
 });

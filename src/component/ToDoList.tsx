932a18971c43095dfc785ddb7b2d62d7a0d3ac76
june <june.korbiz@gmail.com>
2024-05-15 21:32:33 +0900

    feat todolist category button

--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -4,20 +4,21 @@ import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  return (
-    <div>
-      <h1>To Dos</h1>
-      <hr />
-      <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          // <ToDo text={toDo.text} id={toDo.id category={toDo.category}} />
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-    </div>
-  );
+    const toDos = useRecoilValue(toDoState);
+    console.log(toDos);
+    return (
+        <div>
+            <h1>To Dos</h1>
+            <hr />
+            <CreateToDo />
+            <ul>
+                {toDos.map((toDo) => (
+                    // <ToDo text={toDo.text} id={toDo.id category={toDo.category}} />
+                    <ToDo key={toDo.id} {...toDo} />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ToDoList;
